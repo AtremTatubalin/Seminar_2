65b545bca2f7a22e13b78f4e36fb045291174d76
Artyom <tatubalin.artem@mail.ru>
2022-12-20 22:04:10 +0800

    tusk_10

--- tusk_10/Program.cs
+++ tusk_10/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+int ThirdDigit(int number)
+{
+    int digit3 = (number / 10) % 10;
+    return digit3;
+}
+
+Console.WriteLine("Введите трёхзначное число.");
+
+int num = int.Parse(Console.ReadLine());
+int digit = ThirdDigit(num);
+
+Console.WriteLine($"Вторая цифра числа {num} это {digit}");
