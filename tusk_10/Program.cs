4e85626d308e926ded6591c85559bb892634d2b7
Artyom <tatubalin.artem@mail.ru>
2022-12-20 21:41:11 +0800

    tusk_10

---
+++ tusk_10/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+
+int GetNumberFromRange(int start, int end)
+{
+    int number = new Random().Next(start, end + 1);
+    return number;
+}
+
+int GetMaxDigit(int number)
+{
+    int max = number % 10;
+    int digit2 = number / 10;
+
+    if (max < digit2)
+    {
+        max = digit2;
+    }
+    return max;
+}
+
+int randomNumber = GetNumberFromRange(10, 99);
+int maxDigit = GetMaxDigit(randomNumber);
+
+Console.WriteLine($"Для числа {randomNumber} максимальной цифрой является {maxDigit}");
