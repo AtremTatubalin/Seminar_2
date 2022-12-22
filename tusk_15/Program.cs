8206437d1d0d0e2bce4ba43f3d05569af5060942
Artyom <tatubalin.artem@mail.ru>
2022-12-22 22:26:55 +0800

    tusk_15

---
+++ tusk_15/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+Console.Clear();
+
+int number = int.Parse(Console.ReadLine()!);
+
+
+if (number < 8)
+{
+    if (number < 6)
+    {
+        Console.WriteLine("Нет");
+    }
+    else
+    {
+        Console.WriteLine("Да");
+    }
+}
+else
+{
+    Console.WriteLine("неверное число.");
+}
